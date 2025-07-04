34ed81b883526f2c5f126b4be50cf45a2f22140e
forlen30 <sdforlen@gmail.com>
2025-07-05 05:26:43 +0700

    new version 69

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
-const CACHE_NAME = 'My-Familiars-v68'; // เปลี่ยนชื่อเวอร์ชันทุกครั้งที่อัปเดต
+const CACHE_NAME = 'My-Familiars-v69'; // เปลี่ยนชื่อเวอร์ชันทุกครั้งที่อัปเดต
 
 const ASSETS = [
   '/',
@@ -92,7 +92,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting(); 
+
 });
 
 self.addEventListener('activate', event => {
